97359f1fac1179e2ce95b476867ca250af92a0d7
Dyphine Eunice Anyanga <dyphineanyanga@gmail.com>
2025-06-07 06:13:47 +0300

    Update models.py

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -2,17 +2,20 @@ import uuid
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Custom User model extending AbstractUser
 class User(AbstractUser):
     user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     email = models.EmailField(unique=True)
     phone_number = models.CharField(max_length=20, blank=True, null=True)
     
+    # password field is inherited from AbstractUser
+    # no need to redefine unless you want custom behavior
+
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
 
     def __str__(self):
         return self.email
+
 
 
 class Conversation(models.Model):
