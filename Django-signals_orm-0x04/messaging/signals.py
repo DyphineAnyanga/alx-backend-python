05af38a632c0191cb624bb569c0ea86c6b79595a
dyphine anyanga <dyphineanyanga@gmail.com>
2025-06-11 05:35:04 +0300

    Created Models and Signal for Message Edit Tracking

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,11 +1,21 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import pre_save
 from django.dispatch import receiver
-from .models import Message, Notification
+from django.utils import timezone
+from .models import Message, MessageHistory
 
-@receiver(post_save, sender=Message)
-def create_notification(sender, instance, created, **kwargs):
-    if created:
-        Notification.objects.create(
-            user=instance.receiver,
-            message=instance
-        )
+@receiver(pre_save, sender=Message)
+def log_message_edit(sender, instance, **kwargs):
+    if instance.pk:
+        try:
+            original = Message.objects.get(pk=instance.pk)
+            if original.content != instance.content:
+                # Create a history entry before the message is updated
+                MessageHistory.objects.create(
+                    message=original,
+                    old_content=original.content,
+                    edited_by=instance.edited_by,
+                )
+                instance.edited = True
+                instance.edited_at = timezone.now()
+        except Message.DoesNotExist:
+            pass
