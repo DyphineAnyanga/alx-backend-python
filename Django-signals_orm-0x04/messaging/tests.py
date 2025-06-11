2cf9cbeccb6ac885b35dbea60f8d1184c386fada
dyphine anyanga <dyphineanyanga@gmail.com>
2025-06-11 05:00:19 +0300

    Task 0: Initialized Django project and added messaging app to INSTALLED_APPS

---
+++ Django-signals_orm-0x04/messaging/tests.py
@@ -0,0 +1,18 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Message, Notification
+
+class MessagingSignalTestCase(TestCase):
+    def setUp(self):
+        self.sender = User.objects.create_user(username='sender', password='pass')
+        self.receiver = User.objects.create_user(username='receiver', password='pass')
+
+    def test_notification_created_on_message_send(self):
+        message = Message.objects.create(
+            sender=self.sender,
+            receiver=self.receiver,
+            content="Hello there!"
+        )
+        notification = Notification.objects.get(message=message)
+        self.assertEqual(notification.user, self.receiver)
+        self.assertFalse(notification.is_read)
